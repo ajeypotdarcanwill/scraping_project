48d5765fe0f915a850d6cd6203a8f20860bf2f72
Janmejay Rai <janmejay.rai@rsystems.com>
2015-06-26 21:21:48 +0530

    fetch mails

--- app/models/business_enquiry.rb
+++ app/models/business_enquiry.rb
@@ -1,14 +1,25 @@
 class BusinessEnquiry < ActiveRecord::Base
 
-	def send_business_enquiry
+	def self.send_business_enquiry
 		agent = Mechanize.new
-		url = "http://www.yellowpages.ca/bus/Ontario/Mississauga/Gabriel-s-Restaurant-Bar-Grill/2280895.html?what=restaurants&where=Mississauga,+ON&useContext=true"
+		# url = "http://www.yellowpages.ca/bus/Ontario/Mississauga/Gabriel-s-Restaurant-Bar-Grill/2280895.html?what=restaurants&where=Mississauga,+ON&useContext=true"
 		BusinessUrl.all.each do |url|
 			agent.get(url)
 			page = agent.page
 			link = page.link_with(text: "Email")
 			if link.present?
 			end
+		end
+	end
+
+	def self.fetch_email(date)
+		date = Date.parse(date) if date.is_a?(String)
+		gmail = Gmail.connect!(Constants::EMAIL_USERNAME, Constants::EMAIL_PASSWORD)
+		emails = gmail.inbox.find(:unread, on: date, from: Constants::FROM_EMAIL)
+		email.each do |email|
+			message = email.message
+			business_email_address = message.to.first
+			email_body = message.body
 		end
 	end
 end
---
+++ app/models/constants.rb
@@ -0,0 +1,5 @@
+class Constants
+	EMAIL_USERNAME = ""
+	EMAIL_PASSWORD = ""
+	FROM_EMAIL = "no-reply@yellowpages.ca"
+end
