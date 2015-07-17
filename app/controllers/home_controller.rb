c75d53eb0bb14936c6935db3197aedc33a730043
ajey <ajey.potdar@gmail.com>
2015-07-17 13:03:27 +0530

    contants for email user/password

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,6 +15,7 @@ class HomeController < ApplicationController
     end
 	end
 	
+	# to send multiple business enquiries, sends enquiries to all urls where enquiry_sent is false.
 	def send_enquiries
 	 BusinessEnquiry.send_business_enquiry
 	 redirect_to root_url, notice: "Business enquiries sent to the urls where email address was present."
@@ -22,7 +23,7 @@ class HomeController < ApplicationController
 	
 	# Fetch to-emails from gmail received emails from yp.com
 	def fetch_emails
-	 BusinessEnquiry.fetch_email("michaelstarc1984@gmail.com", "michael@12345")
+	 BusinessEnquiry.fetch_email(Constants::GMAIL_USER, Constants::GMAIL_PASSWORD)
 	 redirect_to '/', notice: "Emails fetched."
 	end
 	
@@ -32,11 +33,13 @@ class HomeController < ApplicationController
    send_data file, :type => "application/vnd.openxmlformates-officedocument.spreadsheetml.sheet", filename: "business_enquiries_#{Date.today}.xlsx"
 	end
 	
+	# accepts uploaded csv file and imports business urls from csv file to database.
 	def import_data
 	 BusinessEnquiry.import(params[:file])
 	 redirect_to business_urls_path, notice: "Business urls imported."
 	end
 	
+	# Business urls page, where urls are separately shown sent/to-be-sent.
 	def business_urls
 	 @unsent_urls = BusinessUrl.where(enquiry_sent: false)
 	 @sent_urls = BusinessUrl.where(enquiry_sent: true)
